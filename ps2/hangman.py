d303795149231dbeae1a473208ba0bcaf91ae471
yogeshmanghnani <yamanghnani@gmail.com>
2017-11-11 12:38:28 +0530

    Bugs removed in the hangman() function

--- ps2/hangman.py
+++ ps2/hangman.py
@@ -1,14 +1,6 @@
-# Problem Set 2, hangman.py
-# Name: 
-# Collaborators:
-# Time spent:
 
 # Hangman Game
-# -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
+
 import random
 import string
 
