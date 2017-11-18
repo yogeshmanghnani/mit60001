fc230b0c80eb967bbbee594f140219c304010c8e
yogeshmanghnani <yamanghnani@gmail.com>
2017-11-18 22:56:55 +0530

    update hand function implemented

--- ps3/ps3.py
+++ ps3/ps3.py
@@ -16,7 +16,8 @@ CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
 
 SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1,
+    'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 # -----------------------------------
@@ -25,6 +26,7 @@ SCRABBLE_LETTER_VALUES = {
 
 WORDLIST_FILENAME = "words.txt"
 
+
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
@@ -32,7 +34,7 @@ def load_words():
     Depending on the size of the word list, this function may
     take a while to finish.
     """
-    
+
     print("Loading word list from file...")
     # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r')
@@ -43,6 +45,7 @@ def load_words():
     print("  ", len(wordlist), "words loaded.")
     return wordlist
 
+
 def get_frequency_dict(sequence):
     """
     Returns a dictionary where the keys are elements of the sequence
@@ -52,13 +55,13 @@ def get_frequency_dict(sequence):
     sequence: string or list
     return: dictionary
     """
-    
+
     # freqs: dictionary (element_type -> int)
     freq = {}
     for x in sequence:
-        freq[x] = freq.get(x,0) + 1
+        freq[x] = freq.get(x, 0) + 1
     return freq
-	
+
 
 # (end of helper code)
 # -----------------------------------
@@ -106,6 +109,7 @@ def get_word_score(word, n):
 
     return score
 
+
 #
 # Make sure you understand how this function works and what it does!
 #
@@ -121,11 +125,12 @@ def display_hand(hand):
 
     hand: dictionary (string -> int)
     """
-    
+
     for letter in hand.keys():
         for j in range(hand[letter]):
-             print(letter, end=' ')      # print all on the same line
-    print()                              # print an empty line
+            print(letter, end=' ')  # print all on the same line
+    print()                         # print an empty line
+
 
 #
 # Make sure you understand how this function works and what it does!
@@ -144,19 +149,20 @@ def deal_hand(n):
     n: int >= 0
     returns: dictionary (string -> int)
     """
-    
-    hand={}
+
+    hand = {}
     num_vowels = int(math.ceil(n / 3))
 
     for i in range(num_vowels):
         x = random.choice(VOWELS)
         hand[x] = hand.get(x, 0) + 1
-    
-    for i in range(num_vowels, n):    
+
+    for i in range(num_vowels, n):
         x = random.choice(CONSONANTS)
         hand[x] = hand.get(x, 0) + 1
-    
+
     return hand
+
 
 #
 # Problem #2: Update a hand by removing letters
@@ -179,8 +185,16 @@ def update_hand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
+    temp_hand = hand.copy()
+    for char in word.lower():
+        if char in temp_hand.keys():
+            temp_hand[char] -= 1
+            if temp_hand[char]<0:
+                temp_hand[char]=0
+    return temp_hand
 
-    pass  # TO DO... Remove this line when you implement this function
+
+
 
 #
 # Problem #3: Test word validity
@@ -199,6 +213,7 @@ def is_valid_word(word, hand, word_list):
 
     pass  # TO DO... Remove this line when you implement this function
 
+
 #
 # Problem #5: Playing a hand
 #
@@ -209,11 +224,11 @@ def calculate_handlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    
+
     pass  # TO DO... Remove this line when you implement this function
 
-def play_hand(hand, word_list):
 
+def play_hand(hand, word_list):
     """
     Allows the user to play the given hand, as follows:
 
@@ -242,39 +257,38 @@ def play_hand(hand, word_list):
       returns: the total score for the hand
       
     """
-    
+
     # BEGIN PSEUDOCODE <-- Remove this comment when you implement this function
     # Keep track of the total score
-    
+
     # As long as there are still letters left in the hand:
-    
-        # Display the hand
-        
-        # Ask user for input
-        
-        # If the input is two exclamation points:
-        
-            # End the game (break out of the loop)
 
-            
-        # Otherwise (the input is not two exclamation points):
+    # Display the hand
 
-            # If the word is valid:
+    # Ask user for input
 
-                # Tell the user how many points the word earned,
-                # and the updated total score
+    # If the input is two exclamation points:
 
-            # Otherwise (the word is not valid):
-                # Reject invalid word (print a message)
-                
-            # update the user's hand by removing the letters of their inputted word
-            
+    # End the game (break out of the loop)
+
+
+    # Otherwise (the input is not two exclamation points):
+
+    # If the word is valid:
+
+    # Tell the user how many points the word earned,
+    # and the updated total score
+
+    # Otherwise (the word is not valid):
+    # Reject invalid word (print a message)
+
+    # update the user's hand by removing the letters of their inputted word
+
 
     # Game is over (user entered '!!' or ran out of letters),
     # so tell user the total score
 
     # Return the total score as result of function
-
 
 
 #
@@ -308,10 +322,10 @@ def substitute_hand(hand, letter):
     letter: string
     returns: dictionary (string -> int)
     """
-    
+
     pass  # TO DO... Remove this line when you implement this function
-       
-    
+
+
 def play_game(word_list):
     """
     Allow the user to play a series of hands
@@ -342,9 +356,8 @@ def play_game(word_list):
 
     word_list: list of lowercase strings
     """
-    
-    print("play_game not implemented.") # TO DO... Remove this line when you implement this function
-    
+
+    print("play_game not implemented.")  # TO DO... Remove this line when you implement this function
 
 
 #
